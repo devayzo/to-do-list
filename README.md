bbc71e469a7cba36bd45867d6020fd4e3e547b1a
André Yuri Shinya <andreshinya22@gmail.com>
2026-08-15 13:57:38 -0300

    Uploaded project

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 # to-do-list
 A simple and interactive **To-Do List application built with React and JavaScript**, developed as part of my studies in front-end development.
 
@@ -115,3 +116,21 @@ I plan to continue improving the project as I learn new technologies and concept
 ---
 
 ⭐ This project is part of my programming studies and documents my progress as a developer.
+=======
+# React + Vite
+
+This template provides a minimal setup to get React working in Vite with HMR and some ESLint rules.
+
+Currently, two official plugins are available:
+
+- [@vitejs/plugin-react](https://github.com/vitejs/vite-plugin-react/blob/main/packages/plugin-react) uses [Oxc](https://oxc.rs)
+- [@vitejs/plugin-react-swc](https://github.com/vitejs/vite-plugin-react/blob/main/packages/plugin-react-swc) uses [SWC](https://swc.rs/)
+
+## React Compiler
+
+The React Compiler is not enabled on this template because of its impact on dev & build performances. To add it, see [this documentation](https://react.dev/learn/react-compiler/installation).
+
+## Expanding the ESLint configuration
+
+If you are developing a production application, we recommend using TypeScript with type-aware lint rules enabled. Check out the [TS template](https://github.com/vitejs/vite/tree/main/packages/create-vite/template-react-ts) for information on how to integrate TypeScript and [`typescript-eslint`](https://typescript-eslint.io) in your project.
+>>>>>>> bd4137d (Uploaded project)
---
+++ src/App.jsx
@@ -0,0 +1,97 @@
+import { useState } from 'react'
+import Todo from './components/Todo'
+import TodoForm from './components/todoForm'
+import './App.css'
+import Search from './components/Search'
+import Filter from './components/Filter'
+
+function App() {
+  const [todos, setTodos] = useState([
+    {
+      id: 1,
+      text: "Criar funcionalidade x no sistema",
+      category: "Trabalho",
+      isCompleted: false,
+    },
+    {
+      id: 2,
+      text: "Ir pra academia",
+      category: "Pessoal",
+      isCompleted: false,
+    },
+    {
+      id: 3,
+      text: "Estudar React",
+      category: "Estudos",
+      isCompleted: false,
+    }
+  ])
+
+  const [search, setSearch] = useState("");
+
+  const [filter, setFilter] = useState("All");
+  const [sort, setSort] = useState("Asc");
+
+  const addTodo = (text, category) => {
+    const newTodos = [...todos, {
+      id: Math.floor(Math.random() * 10000),
+      text,
+      category,
+      isCompleted: false
+    },
+  ];
+
+    setTodos(newTodos);
+  }
+  
+  const removeTodo = (id) => {
+    setTodos((filteredTodos) =>
+      filteredTodos.filter((todo) => todo.id !== id)
+    );
+  }
+
+  const completeTodo = (id) => {
+    const newTodos = [...todos];
+    newTodos.map((todo) =>
+      todo.id === id ? (todo.isCompleted = !todo.isCompleted) : todo,
+    );
+    setTodos(newTodos);
+  };
+ 
+  return (
+    <div className="app">
+      <Search search={search} setSearch={setSearch} />
+      <Filter filter={filter} setFilter={setFilter} setSort={setSort} />
+      <h1>Lista de tarefas</h1>
+      <div className="todo-list">
+        {todos
+          .filter((todo) =>
+            filter === "All"
+              ? true
+              : filter === "Completed"
+                ? todo.isCompleted
+                : !todo.isCompleted,
+          )
+          .filter((todo) =>
+            todo.text.toLowerCase().includes(search.toLowerCase()),
+          )
+          .sort((a, b) =>
+            sort === "Asc"
+              ? a.text.localeCompare(b.text)
+              : b.text.localeCompare(a.text),
+          )
+          .map((todo) => (
+            <Todo
+              key={todo.id}
+              todo={todo}
+              removeTodo={removeTodo}
+              completeTodo={completeTodo}
+            />
+          ))}
+      </div>
+      <TodoForm addTodo={addTodo} />
+    </div>
+  );
+}
+
+export default App
